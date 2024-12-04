45456b2b02699c1d9e724d102c9770d30256a513
RobinK <robin.kilbertus@gmail.com>
2024-12-04 09:28:58 +0100

    add: fetch cookie

--- frontend/src/redux/adminStore.js
+++ frontend/src/redux/adminStore.js
@@ -1,15 +1,18 @@
 import { configureStore, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import getCookie from '../services/getCookie';
 
 const ENV_URL_API = 'http://localhost:8000/api';
-
-const BEARER_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MzI0Njk2MDUsImV4cCI6MTczMjQ3MzIwNSwicm9sZXMiOlsiUk9MRV9VU0VSIiwiUk9MRV9BRE1JTiJdLCJlbWFpbCI6IlJvYmxlWm9iQHpvYi5jb20ifQ.RJ7GRvTStl4QkBbMnnw5miH-5Tnzz6Z-qhVyyt9foOQv9Me1eGu7ZMJiGmd2HRUGU2v59RtOd1fZAR6Iw1tgDlLgcOyIyqKaXslGtX5TX04itXzzUrYzwgB5onzjghYcPJffdsjyR8UZdALLBIF9AFWKQThAvo5Ze14vIE5lkgR0kVn7itLg2ePrfJQzpZTw8WoSRqX7sW4t0lvHpZwhtwUE-es9zWQpMCVh9zTeKMNbOElr3izjVhScfWVsrwFLw7R0L7G1LIhwvXk4CalO2kuPW2NuUvi3AsxB4YhDtF0DyaoU8XetowoIzum4mzAoNGVyHO_lk-GdmELuHSvNGQ';
 
 export const fetchUnvalidatedImages = createAsyncThunk(
   'app/fetchUnvalidatedImages',
   async () => {
+    const token = getCookie('token');
+    if (!token) {
+      throw new Error('Token introuvable dans les cookies');
+    }
     const response = await fetch(`${ENV_URL_API}/images/getOnlyUnalidated`, {
       headers: {
-        Authorization: `Bearer ${BEARER_TOKEN}`,
+        Authorization: `Bearer ${token}`,
       },
     });
     if (!response.ok) {
@@ -19,6 +22,7 @@ export const fetchUnvalidatedImages = createAsyncThunk(
     return data;
   }
 );
+
 
 const appSlice = createSlice({
   name: 'app',
